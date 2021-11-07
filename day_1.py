15c0edf7761ba9073d3be884dfb3da5b2387f966
Your Name <you@example.com>
2021-11-07 11:45:03 +0100

    day 1 variables

---
+++ day_1.py
@@ -0,0 +1,54 @@
+#learning print
+# print('Day 1 - Python Print Function')
+# print('The function is declared like this:')
+# print("print('what to print')")
+
+#string parameters in python
+#new line
+# print("Hello world! \nHello World\n")
+#concatinate
+
+# print("Hello" + " " + "Yurii")
+
+#debuging exercise
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+#input function and length of the sting
+# print(len(input("What is your name?\n")))
+
+#variables
+# user_name = input("What is your name?\n")
+#length = len(name)
+# print(length)
+
+# 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# c = a
+# a = b
+# b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+inputss = "5"
+print(inputss)
+
+
+# day_1_project
+greeting = "Welcome to the Brand Name Generator"
+print(greeting)
+city = input("What's name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+print("Your brand name could be" + " " + city + " " + pet);
